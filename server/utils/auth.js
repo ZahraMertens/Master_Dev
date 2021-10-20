4c11c414fc8e5b6792815bc8810fc25dec79e35a
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-10-20 17:51:02 +1100

    Add utils folder and auth file for jwt

---
+++ server/utils/auth.js
@@ -0,0 +1,13 @@
+// const jwt = require('jsonwebtoken');
+
+// const secret = 'seeeccreeet';
+// const expiration = '2h';
+
+// module.exports = {
+//   signToken: function ({ email, username, _id }) {
+//     const payload = { email, username, _id };
+//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+//   },
+// };
+
+//QUESTION: TOEKN FOR STUDENT AND TUTOR OR ONLY ONE?
