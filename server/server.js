f1fa547a9c41d9ac979e22949734b3628b02695b
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-10-20 14:56:19 +1100

    Update server.js and add apollo express

--- server/server.js
+++ server/server.js
@@ -9,10 +9,14 @@ const express = require("express");
 const { ApolloServer } = require('apollo-server-express');
 const path = require("path");
 const db = require("./config/connection");
-const routes = require("./routes");
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,10 +25,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`Now listening on localhost: ${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
 
 // STRIPE
