ce0af8a0b4917d3bea235eeab62b30ee3ed5a170
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-10-21 14:46:14 +1100

    Add auth for login to client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from "./pages/Home/index";
 import Header from "./components/Header/index";
 import Footer from "./components/Footer/index";
 
+//Main graphql endpoint
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 function App() {
