7c37fb8bbb45cdb55a65e5176592ce7a8d3c05e0
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-10-21 18:16:39 +1100

    Add comments to guide functionality for login page

---
+++ client/src/pages/TutorLogin/index.js
@@ -0,0 +1,9 @@
+//const {loading, data, error } = useQuery(Query from auth.js)
+// loading = when accesing to db we track te prgress
+// when loading finished loading= false
+// data = array of objects of data from db
+//error = undefined no error
+
+//Mutation
+// const [addProfile, {error, data}] = useMutation(ADD_PROFILE)
+//then call function in handle form submit
