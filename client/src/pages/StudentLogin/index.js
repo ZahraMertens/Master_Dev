38aab31d75841eaea36d10d9d494d261cca70f37
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-10-22 11:05:32 +1100

    Add tutor and student signup components

--- client/src/pages/StudentLogin/index.js
+++ client/src/pages/StudentLogin/index.js
@@ -1,9 +1,115 @@
-import "./studentlogin.css"
+import "./studentlogin.css";
 
 export default function StudentLogin() {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div class="studentsignup-main">
+        <div class="signup-wrapper">
+          <div class="row">
+            <div class="col">
+              <form class="row">
+                <div class="col-12">
+                  <h1>Student Sign Up</h1>
+                </div>
+                <div class="col-6">
+                  <label for="validationCustom01" class="form-label">
+                    First name
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="Mark"
+                    required
+                  />
+                </div>
+                <div class="col-6">
+                  <label for="validationCustom01" class="form-label">
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="Doe"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <label for="validationCustom01" class="form-label">
+                    Phone Number
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="0123123123"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <label for="validationCustom01" class="form-label">
+                    mail address
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="mark.doe@test.com"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <label for="validationCustom01" class="form-label">
+                    Create Password
+                  </label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="Mark1234"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <label for="validationCustom01" class="form-label">
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="Mark1234"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <div class="form-check">
+                    <input
+                      class="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="invalidCheck"
+                      required
+                    />
+                    <label class="form-check-label" for="invalidCheck">
+                      Agree to terms and conditions
+                    </label>
+                    <div class="invalid-feedback">
+                      You must agree before submitting.
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <button class="btn btn-primary" type="submit">
+                    Submit form
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- client/src/pages/TutorLogin/index.js
+++ client/src/pages/TutorLogin/index.js
@@ -8,12 +8,156 @@
 // const [addProfile, {error, data}] = useMutation(ADD_PROFILE)
 //then call function in handle form submit
 
-import "./tutorlogin.css"
+import "./tutorlogin.css";
 
 export default function TutorLogin() {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div class="tutorsignup-main">
+        <div class="signup-wrapper">
+          <div class="row">
+            <div class="col">
+              <form class="row">
+                <div class="col-12">
+                  <h1>Tutor Sign Up</h1>
+                </div>
+                <div class="col-6">
+                  <label for="validationCustom01" class="form-label">
+                    First name
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="Mark"
+                    required
+                  />
+                </div>
+                <div class="col-6">
+                  <label for="validationCustom01" class="form-label">
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="Doe"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <label for="validationCustom01" class="form-label">
+                    Phone Number
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="0123123123"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <label for="validationCustom01" class="form-label">
+                    Email address
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="mark.doe@test.com"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <label for="validationCustom01" class="form-label">
+                    Degree
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="Master in Computer Science"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <label for="validationCustom01" class="form-label">
+                    Hourly Rate $
+                  </label>
+                  <div class="input-group mb-3">
+                    <span class="input-group-text">$</span>
+                    <input
+                      type="text"
+                      class="form-control"
+                      aria-label="Amount (to the nearest dollar)"
+                    />
+                    <span class="input-group-text">.00</span>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <label for="validationCustom01" class="form-label">
+                    About Me (Short Describtion)
+                  </label>
+                  <textarea
+                    type="text"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="mark.doe@test.com"
+                    required
+                  ></textarea>
+                </div>
+                <div class="col-12">
+                  <label for="validationCustom01" class="form-label">
+                    Create Password
+                  </label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="Mark1234"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <label for="validationCustom01" class="form-label">
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="validationCustom01"
+                    value="Mark1234"
+                    required
+                  />
+                </div>
+                <div class="col-12">
+                  <div class="form-check">
+                    <input
+                      class="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="invalidCheck"
+                      required
+                    />
+                    <label class="form-check-label" for="invalidCheck">
+                      Agree to terms and conditions
+                    </label>
+                    <div class="invalid-feedback">
+                      You must agree before submitting.
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <button class="btn btn-primary" type="submit">
+                    Submit form
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
