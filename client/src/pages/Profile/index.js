677f6dbf1d15e3c1e81651891947a7c218755652
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-10-24 14:43:57 +1100

    Add getone user query to frontend to be able to display the tutors profile

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import { TUTOR_BY_ID } from '../../utils/queries';
 
 export default function Profile () {
+
+    const { tutorId } = useParams();
+
+    const { loading, data }= useQuery(TUTOR_BY_ID, {
+        variables: { tutorId: tutorId},
+    })
+
+    const tutor = data?.onetutor || {};
+
     return (
         <div>
+            {/* DISPLAY THE WHOLE TUTOR DATA */}
             <h1>Test Header</h1>
         </div>
     )
