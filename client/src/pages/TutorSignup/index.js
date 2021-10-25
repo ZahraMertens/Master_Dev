9f3763d549bcafe17edbceb15dfdf10776628592
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-10-25 15:49:41 +1100

    Add tutor signup functionality to frontend

--- client/src/pages/TutorSignup/index.js
+++ client/src/pages/TutorSignup/index.js
@@ -10,14 +10,67 @@
 
 import "./tutorsignup.css";
 
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { useMutation } from '@apollo/client';
+import { ADD_TUTOR } from '../../utils/mutations';
+
+import Auth from '../../utils/auth';
+
 export default function TutorSignup() {
+
+  const [formState, setFormState] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    phone: '',
+    describtion: '',
+    language: '',
+    degree: '',
+    hourRate: '',
+    userType: "Tutor",
+  });
+
+  const [addTutor, { error, data }] = useMutation(ADD_TUTOR);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await addTutor({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addTutor.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
     <div>
       <div className="tutorsignup-main">
         <div className="signup-wrapper">
           <div className="row form-tutorsignup">
             <div className="col">
-              <form className="row">
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              <form className="row" onSubmit={handleFormSubmit}>
                 <div className="col-12">
                   <h1>Tutor Sign Up</h1>
                 </div>
@@ -29,7 +82,10 @@ export default function TutorSignup() {
                     type="text"
                     className="form-control"
                     id="validationCustom01"
-                    value="Mark"
+                    placeholder="Mark"
+                    name="firstName"
+                    value={formState.firstName}
+                    onChange={handleChange}
                     required
                   />
                 </div>
@@ -41,7 +97,10 @@ export default function TutorSignup() {
                     type="text"
                     className="form-control"
                     id="validationCustom01"
-                    value="Doe"
+                    placeholder="Doe"
+                    name="lastName"
+                    value={formState.lastName}
+                    onChange={handleChange}
                     required
                   />
                 </div>
@@ -53,7 +112,10 @@ export default function TutorSignup() {
                     type="text"
                     className="form-control"
                     id="validationCustom01"
-                    value="0123123123"
+                    placeholder="0423123456"
+                    name="phone"
+                    value={formState.phone}
+                    onChange={handleChange}
                     required
                   />
                 </div>
@@ -65,7 +127,10 @@ export default function TutorSignup() {
                     type="text"
                     className="form-control"
                     id="validationCustom01"
-                    value="mark.doe@test.com"
+                    placeholder="john.doe@gmail.com"
+                    name="email"
+                    value={formState.email}
+                    onChange={handleChange}
                     required
                   />
                 </div>
@@ -77,7 +142,25 @@ export default function TutorSignup() {
                     type="text"
                     className="form-control"
                     id="validationCustom01"
-                    value="Master in Computer Science"
+                    placeholder="Bachelor in Computer Science"
+                    name="degree"
+                    value={formState.degree}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-12">
+                  <label htmlFor="validationCustom01" className="form-label">
+                    Programming Language
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="validationCustom01"
+                    placeholder="JavaScript"
+                    name="language"
+                    value={formState.language}
+                    onChange={handleChange}
                     required
                   />
                 </div>
@@ -91,6 +174,11 @@ export default function TutorSignup() {
                       type="text"
                       className="form-control"
                       aria-label="Amount (to the nearest dollar)"
+                      placeholder="30"
+                      name="hourRate"
+                      value={formState.hourRate}
+                      onChange={handleChange}
+                      required
                     />
                     <span className="input-group-text">.00</span>
                   </div>
@@ -103,31 +191,24 @@ export default function TutorSignup() {
                     type="text"
                     className="form-control"
                     id="validationCustom01"
-                    value="mark.doe@test.com"
+                    placeholder="Your professional describtion and background"
+                    name="describtion"
+                    value={formState.describtion}
+                    onChange={handleChange}
                     required
                   ></textarea>
                 </div>
                 <div className="col-12">
                   <label htmlFor="validationCustom01" className="form-label">
-                    Create Password
+                    Password
                   </label>
                   <input
                     type="password"
                     className="form-control"
                     id="validationCustom01"
-                    value="Mark1234"
-                    required
-                  />
-                </div>
-                <div className="col-12">
-                  <label htmlFor="validationCustom01" className="form-label">
-                    Confirm Password
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="validationCustom01"
-                    value="Mark1234"
+                    name="password"
+                    value={formState.password}
+                    onChange={handleChange}
                     required
                   />
                 </div>
@@ -154,6 +235,13 @@ export default function TutorSignup() {
                   </button>
                 </div>
               </form>
+              )}
+
+              {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                Oooops, something went wrong! Please try again...
+              </div>
+              )} 
             </div>
           </div>
         </div>
