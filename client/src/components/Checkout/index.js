ec36155b8df0c8103d84c2a33eb837615e67c545
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-11-07 23:29:40 +1100

    Remove aws keys

--- client/src/components/Checkout/index.js
+++ client/src/components/Checkout/index.js
@@ -12,7 +12,7 @@ import Auth from "../../utils/auth";
 
 // stripePromise returns a promise with the stripe object as soon as the Stripe package loads
 const stripePromise = loadStripe(
-  process.env.REACT_APP_STRIPE_PK
+  `${process.env.REACT_APP_STRIPE_PK}`
 );
 
 const Cart = ({ tutor }) => {
